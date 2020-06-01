57272fd290559f9bbf0c3620606e3fa6abe9aaff
jueoneom <lluot.llu@gmail.com>
2020-06-01 16:53:42 +0900

    add navigationbar

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import numpy as np 
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 form_class=uic.loadUiType("test.ui")[0]
 
@@ -11,6 +13,9 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.figure=Figure(figsize=(6,7))
+        self.canvas=FigureCanvas(self.figure)
+        self.toolBar=NavigationToolbar(self.canvas, self)
         # self.okBtn.clicked.connect(self.btn_clicked)
     
     def btn_clicked(self):
